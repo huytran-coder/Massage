87318d2d6ae986300c8be144ed6cd16fa468bc11
huytran-coder <tranhuyhv303@gmail.com>
2024-06-11 20:03:46 +0700

    commit massage

---
+++ Messenger-main/messenger/app/src/main/java/com/example/easychat/LoginUsernameActivity.java
@@ -0,0 +1,108 @@
+package com.example.easychat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.easychat.model.UserModel;
+import com.example.easychat.utils.FirebaseUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginUsernameActivity extends AppCompatActivity {
+
+    EditText usernameInput;
+    Button letMeInBtn;
+    ProgressBar progressBar;
+    String phoneNumber;
+    UserModel userModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_username);
+
+        usernameInput = findViewById(R.id.login_username);
+        letMeInBtn = findViewById(R.id.login_let_me_in_btn);
+        progressBar =findViewById(R.id.login_progress_bar);
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+        getUsername();
+
+        letMeInBtn.setOnClickListener((v -> {
+            setUsername();
+        }));
+
+
+    }
+
+    void setUsername(){
+
+        String username = usernameInput.getText().toString();   
+        if(username.isEmpty() || username.length()<3){
+            usernameInput.setError("Tên người dùng phải có nhiều hơn 3 kí tự");
+            return;
+        }
+        setInProgress(true);
+        if(userModel!=null){
+            userModel.setUsername(username);
+        }else{
+            userModel = new UserModel(phoneNumber,username, Timestamp.now(),FirebaseUtil.currentUserId());
+        }
+
+        FirebaseUtil.currentUserDetails().set(userModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginUsernameActivity.this,"Đăng nhập thành công",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginUsernameActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(LoginUsernameActivity.this, "Đăng nhập không thành công", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    void getUsername(){
+        setInProgress(true);
+        FirebaseUtil.currentUserDetails().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                  userModel =    task.getResult().toObject(UserModel.class);
+                 if(userModel!=null){
+                     usernameInput.setText(userModel.getUsername());
+                 }
+                }
+            }
+        });
+    }
+
+    void setInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            letMeInBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            letMeInBtn.setVisibility(View.VISIBLE);
+        }
+    }
+}
